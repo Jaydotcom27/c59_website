3ea98181d4ec0c6157fd3afa95acb5cc3a3b584f
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-09-13 14:57:10 -0400

    Initial Commit

---
+++ frontend_react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex'>
+        <h1>Reacasdt App</h1>
+        <h1>React App</h1>
+        <h1>React App</h1>
+    </div>
+  )
+}
+
+export default App
