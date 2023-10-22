537dbf6e5d1495ca59053739c7326d58e782cb35
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-10-22 13:20:01 -0400

    added media

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+// import reelVideo from './assets/reel.mp4';
 
 const App = () => {
   return (
-    <div className='flex'>
-        <h1>Reacasdt App</h1>
-        <h1>React App</h1>
+    <div>
+      <div className='video-container bg'>
+        <div className='overlap'></div>
+        {/* <video autoPlay muted loop id="myVideo">
+          <source src={reelVideo} type="video/mp4"/>
+        </video> */}
+      </div>
         <h1>React App</h1>
     </div>
   )
